d21c2aef4e9a6fc55d72f288610b7dd3113c8f64
Adrian Wróblewski <xwrobel02@gmail.com>
2023-03-31 12:44:37 +0200

    Update index.js

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ app = express();
 
 const corsConfig = {
     credentials: true,
+    origin: 'https://microblog-wkeo.onrender.com',
+    optionSuccessStatus: 200
 };
 app.use(cors(corsConfig));
 
@@ -18,7 +20,6 @@ app.use(cookieParser())
 
 app.use((req, res, next) => {
     res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE, PATCH');
-    res.append('Access-Control-Allow-Origin', 'https://microblog-wkeo.onrender.com')
     next();
 });
 
